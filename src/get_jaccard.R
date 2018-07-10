77a08bcd8608968d0e92446c39a8431103823e89
MarconiS <s.marconi@ufl.edu>
2018-07-09 22:39:56 -0400

    Create get_jaccard.R
    main function to use to run the Jaccard_index on predicted vs ground truth ITC polygons

---
+++ src/get_jaccard.R
@@ -0,0 +1,46 @@
+get_jaccard <- function(pred_itc_pt=NULL, ground_pt=NULL, dataname=NULL, epsg = NULL){
+  
+  library(rgdal)
+  library(dplyr)
+  library(raster)
+  library(doParallel)
+  library(rlas)
+  library(rgeos)
+  library(clue)
+  library(raster)
+  library(lidR)
+  
+  #### READ IN ground data
+  itcs <- readOGR(dsn=ground_pt, layer = dataname,stringsAsFactors = F)
+  
+  #create dataframe
+  itc_df <- itcs@data
+  itc_df$Id <- as.integer(itc_df$Id)
+  # how many crowns in total? 628
+  
+  listPlot <- list.files("~/Documents/ITCs/TALL_polygons/ITCs/", pattern = ".shp")
+  listPlot <- substr(listPlot,1,nchar(listPlot)-4) 
+  
+  #loop in each prediction polygon
+  jaccard_stats = list()
+  for(i in listPlot){
+    tryCatch({ 
+      print(i)
+      pred_itc <- readOGR(dsn=pred_itc_pt, layer = i,stringsAsFactors = F, verbose = F)
+      proj4string(pred_itc) <-  CRS(paste("+init=epsg:", epsg, sep=""))
+      itcs <-spTransform(itcs, CRS("+init=epsg:32616"))
+      if(!is.null(crop(pred_itc, extent(itcs)))){
+        print("gotcha!")
+        jac.dalponte[[i]] <- overJac(itcs, pred_itc)
+      }
+    }, error=function(e){cat(paste("ERROR in ", i, " :" ,sep="~"),conditionMessage(e), "\n")})
+  }
+}
+
+#debug paths
+pred_itc_pt = "~/Documents/ITCs/TALL_polygons/ITCs/"
+ground_pt = "~/Documents/ITCs/TALL_crown_polygons/"
+dataname = "TALL_sample_crowns_edits_May2018"
+epsg <- 32616
+
+get_jaccard(pred_itc_pt, ground_pt, dataname, epsg)
